63be6a8e5fabbe3699b2883b827e181a33e02d9e
Eric Smallwood <smallwood.eric@ymail.com>
2015-06-06 14:45:20 -0500

    - video added

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Eric on 6/6/2015.
+ */
+
+$(document).ready(function(){
+    swfobject.embedSWF("https://www.youtube.com/v/5vuKg7UB6AM%26hl=en_US%26fs=1%26autoplay=1", 'video-container', '516', '315', '9.0.0');
+});
