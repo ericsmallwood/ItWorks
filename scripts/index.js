d33c7bd8a64bc8b38756203001868527007e004c
Eric Smallwood <smallwood.eric@ymail.com>
2015-06-28 09:32:37 -0500

    - Email functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function(){
 
-
+    //modal video functionality
     var videoModal = $('#video-modal').overlay({
         expose: {
             color: 'black',
@@ -29,5 +29,90 @@ $(document).ready(function(){
         swfobject.removeSWF('video-container');
         //remove swf remoes the container so add it back
         $('#video-modal').append('<div id="video-container"></div>');
+    });
+
+    //email form functionality
+    $('#emailFormSendBtn').click( function(){
+        var sendButton = $(this);
+
+        function showText (element, text, milliseconds){
+            element.html(text);
+            window.setTimeout(function(){
+                element.html("");
+            }, milliseconds);
+        }
+
+        function enableDisableEmailForm(disable){
+            nameInput.prop('disabled', disable);
+            emailInput.prop('disabled', disable);
+            subjectInput.prop('disabled', disable);
+            commentInput.prop('disabled', disable);
+            sendButton.prop('disabled', disable);
+        }
+
+        function validateElement(element){
+            if(element.val() === ""){
+                element.css('background', '#ff9999');
+                return false;
+            }else{
+                element.css('background', 'white');
+                return true;
+            }
+        }
+
+        var emailMessageArea = $('#emailMessageArea');
+
+        //message area hieght should match content area
+        emailMessageArea.css('height', $('#contactContent').css('height'));
+
+        var emailArea = $('#emailArea');
+        var nameInput = $('#emailFormName');
+        var emailInput = $('#emailFormEmailAddress');
+        var subjectInput = $('#emailFormSubject');
+        var commentInput = $('#emailFormComment');
+
+        //disable form elements while submitting
+        enableDisableEmailForm(true);
+
+        //check if form is valid
+        if(validateElement(nameInput) && validateElement(emailInput) && validateElement(commentInput)){
+            //set all inputs background color back to white
+            nameInput.css('background', 'white');
+            emailInput.css('background', 'white');
+            commentInput.css('background', 'white');
+        }else{
+            //re enable fields and show error message
+            enableDisableEmailForm(false);
+            var validationHtml = "<h2 style='color: red'>Please Fill Out All Required Fields and Try Again.</h2>";
+            emailMessageArea.html(validationHtml);
+            return false;
+        }
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "scripts/mail.php",
+            success: function(data){
+                enableDisableEmailForm(false);
+                emailArea.html("<h2>"+data.message+"</h2>");
+                nameInput.val("");
+                emailInput.val("");
+                subjectInput.val("");
+                commentInput.val("");
+                var successMessage = "<h2>Thank You! Your message has been sent!</h2>";
+                showText(emailMessageArea, successMessage, 3000);
+            },
+            error: function(error){
+                enableDisableEmailForm(false);
+                var errorHtml = "<h2 style='color: red'>Server Error. The message was not sent. Please Try again.</h2>";
+                showText(emailMessageArea, errorHtml, 3000);
+            },
+            data: {
+                name: nameInput.val(),
+                email: nameInput.val(),
+                subject: subjectInput.val(),
+                body: commentInput.val()
+            }
+        });
     });
 });
---
+++ scripts/mail.php
@@ -0,0 +1,53 @@
+<?php
+require('../thirdparty/phpmailer/PHPMailerAutoload.php');
+
+if(is_ajax()){
+    $mail = new PHPMailer;
+
+    //server settings
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Port = 587;
+    $mail->SMTPSecure = "tls";
+
+    //login info
+    $mail->Username = 'example';
+    $mail->Password = 'password';
+
+    //disable ssl verification because gmail server configuration is not properly set up to work for
+    //php 5.6
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+
+    //email details
+    $subject = $_POST["subject"];
+    $mail->SetFrom('example@gmail.com', "Website Reply");
+    $mail->addAddress('example@gmail.com', 'Example');
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $body = $_POST["body"];
+    $mail->Body    = '<strong>Name: </strong>'.$name.'<br /><strong>Email: </strong>'.$email.'<br />'.'<strong>Message: </strong>'.$body;
+
+    if(!$mail->send()) {
+        header('HTTP/1.1 500 Internal Server Booboo');
+        header('Content-Type: application/json; charset=UTF-8');
+        die(json_encode(array('message' => 'ERROR', 'code' => 500)));
+    } else {
+        echo json_encode('Thank You! Your message has been sent');
+    }
+}else{
+    echo "You do not have permission to access this page.";
+}
+
+function is_ajax() {
+    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+}
